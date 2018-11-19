711eed14d95e9a95b84fb0e4c85e7fe8e06add29
k_vasilchenko <ekaterina.vasilchenko@nixsolutions.com>
2018-11-19 10:41:15 +0200

    remove unnecessary files

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var kit = require('gulp-kit');
 var browserSync = require('browser-sync').create();
-var prettify = require('gulp-prettify');
 
-gulp.task('serve', ['sass', 'kit', 'prettify'], function () {
+gulp.task('serve', ['sass'], function () {
 
     browserSync.init({
         server: {
             baseDir: ".",
-            index: "index.html"
+            index: "src/index.html"
         }
     });
 
     gulp.watch('./scss/**/*.scss', ['sass']).on('change', browserSync.reload);
-    gulp.watch('./kit/**/*.kit', ['kit']).on('change', browserSync.reload);
-    gulp.watch('./src/**/*.html', ['prettify']).on('change', browserSync.reload);
+    gulp.watch('./src/**/*.html').on('change', browserSync.reload);
 });
 
 
@@ -30,31 +27,8 @@ gulp.task('sass', function () {
 });
 
 
-gulp.task('kit', function () {
-    return gulp
-        .src('./kit/*.kit')
-        .pipe(kit())
-        .pipe(gulp.dest('./src/'))
-});
-
-gulp.task('prettify', function () {
-    return gulp
-        .src('./src/*.html')
-        .pipe(prettify({
-            indent_size: 1,
-            preserve_newlines: true,
-            indent_with_tabs: true,
-            end_with_newline: true,
-            max_preserve_newlines: 0,
-            brace_style: 'expand'
-        }))
-        .pipe(gulp.dest('./'));
-});
-
 gulp.task('watch', function () {
     gulp.watch('./scss/**/*.scss', ['sass']);
-    gulp.watch('./kit/**/*.kit', ['kit']);
-
 });
 
 gulp.task('default', ['serve']);
